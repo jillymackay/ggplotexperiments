818d2e2d5ef0567874001b33d91b0ee417dc7e66
MACKAY Jill <jmackay4@ed.ac.uk>
2020-07-16 17:45:12 +0100

    add mood tracker

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ggplotexperiments
+# ggplot2 experiments
+
+
+Just a repository to dump a lot of charts/attempts at charts I've tried over the years
---
+++ mood_tracker/mood_tracker_vis.R
@@ -0,0 +1,35 @@
+# Moodtracker experiment
+
+
+library(tidyverse)
+library(lubridate)
+library(emojifont)
+library(extrafont)
+library(nord)
+
+
+
+moods <- read.csv("mood_tracker/data/moods.csv") %>% 
+  mutate(date = ymd(date),
+         mood = as.character(mood),
+         mood = parse_factor(mood, levels = c("angry", "ugh", "glum", "meh", "good", "great")),
+         x = sample(x = (1:10), size = 30, replace = T),
+         y = sample(x = (1:30), size = 30, replace = T),
+         label = emoji('star')) %>% 
+  select(-X)
+ 
+
+
+moods %>% 
+  ggplot(aes(x = date, y = mood, colour = mood, label = label)) +
+  geom_text(family = "EmojiOne", size = 15) +
+  theme_void() +
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = "#2E3440", colour ="#2E3440" ),
+        plot.background = element_rect(fill = "#2E3440"),
+        plot.title = element_text(family = "Ink Free", colour = "#8FBCBB", size = 18, hjust = 0.5)) +
+  scale_colour_nord(palette = "aurora") +
+  labs (title = "A Mood Tracker") +
+  #geom_text(aes(label = "https://github.com/jillymackay/ggplotexperiments", x = dmy("2020-06-28"), y = "angry"),
+            family ="Calibri",
+            colour ="#2E3440")
